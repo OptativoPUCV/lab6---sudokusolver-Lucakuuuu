7ad18961f506d476a3023d17708969091abb51a4
Your Name <you@example.com>
2024-05-16 21:31:03 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -45,13 +45,7 @@ void print_node(Node* n){
 
 int is_valid(Node* n)
 {
-   int k=4,p; 
-   for(p=0;p<9;p++){
-       int i=3*(k/3) + (p/3) ;
-       int j=3*(k%3) + (p%3) ;
-       printf("%d ",n->sudo[i][j]);
-       if(p%3 == 2) printf("\n");
-   }
+   
    return 1;
 }
 
